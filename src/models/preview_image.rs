a6b9271e64c05e7e418550c85878c7495f33c4e8
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-10-20 22:09:17 +0200

    Create cache dir if not found

--- src/models/preview_image.rs
+++ src/models/preview_image.rs
@@ -1,8 +1,8 @@
 use base64::encode;
 use reqwest::r#async::Client;
 use std::fs::File;
-use std::io;
 use std::path::PathBuf;
+use std::{fs, io};
 
 lazy_static! {
     static ref CACHE_DIR: PathBuf = glib::get_user_cache_dir().unwrap().join("read-it-later");
@@ -26,11 +26,13 @@ impl PreviewImage {
 
     fn download_cache(&self) {
         if !self.cache.exists() {
-            let mut resp = reqwest::get(&self.url).expect("request failed");
-            let mut out = File::create(self.cache.clone()).expect("failed to create file");
-            io::copy(&mut resp, &mut out).expect("failed to copy content");
-
-            info!("Downloading preview image {} into {:#?}", self.url, self.cache);
+            let cache_dir = &CACHE_DIR;
+            if let Ok(_) = fs::create_dir_all(&cache_dir.to_str().unwrap()) {
+                let mut resp = reqwest::get(&self.url).expect("request failed");
+                let mut out = File::create(self.cache.clone()).expect("failed to create file");
+                io::copy(&mut resp, &mut out).expect("failed to copy content");
+                info!("Downloading preview image {} into {:#?}", self.url, self.cache);
+            }
         }
     }
 
