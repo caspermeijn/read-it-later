0e8ce71ed85a509546fa516f1579cd0a147ae8ce
Casper Meijn <casper@meijn.net>
2022-08-05 20:31:37 +0200

    Cargo: Update secret service

--- src/models/secret.rs
+++ src/models/secret.rs
@@ -1,16 +1,15 @@
 use secret_service::EncryptionType;
-use secret_service::{SecretService, SsError};
+use secret_service::{Error, SecretService};
 use std::collections::HashMap;
-use std::rc::Rc;
 use wallabag_api::types::Config;
 
-pub struct SecretManager {
-    service: Rc<SecretService>,
+pub struct SecretManager<'a> {
+    service: SecretService<'a>,
 }
 
-impl SecretManager {
-    pub fn new() -> Result<Self, SsError> {
-        let service = Rc::new(SecretService::new(EncryptionType::Dh)?);
+impl SecretManager<'_> {
+    pub fn new() -> Result<Self, Error> {
+        let service = SecretService::new(EncryptionType::Dh)?;
         let collection = service.get_default_collection()?;
         if collection.is_locked()? {
             collection.unlock()?;
@@ -18,11 +17,11 @@ impl SecretManager {
         Ok(Self { service })
     }
 
-    pub fn logout(username: &str) -> Result<(), SsError> {
+    pub fn logout(username: &str) -> Result<(), Error> {
         let service = Self::new()?;
 
         let collection = service.service.get_default_collection()?;
-        let items = collection.search_items(vec![("wallabag_username", &username)])?;
+        let items = collection.search_items(HashMap::from([("wallabag_username", username)]))?;
 
         for item in items.iter() {
             item.delete()?;
@@ -30,23 +29,23 @@ impl SecretManager {
         Ok(())
     }
 
-    pub fn store_from_config(config: Config) -> Result<(), SsError> {
+    pub fn store_from_config(config: Config) -> Result<(), Error> {
         let service = Self::new()?;
 
         let collection = service.service.get_default_collection()?;
 
-        let username = config.username.clone();
+        let username = config.username.as_str();
         let mut secret_config = HashMap::new();
-        secret_config.insert("WALLABAG_USERNAME".to_string(), config.username.clone());
-        secret_config.insert("WALLABAG_CLIENT_ID".to_string(), config.client_id);
-        secret_config.insert("WALLABAG_CLIENT_SECRET".to_string(), config.client_secret);
-        secret_config.insert("WALLABAG_PASSWORD".to_string(), config.password);
-        secret_config.insert("WALLABAG_URL".to_string(), config.base_url);
+        secret_config.insert("WALLABAG_USERNAME", config.username.clone());
+        secret_config.insert("WALLABAG_CLIENT_ID", config.client_id);
+        secret_config.insert("WALLABAG_CLIENT_SECRET", config.client_secret);
+        secret_config.insert("WALLABAG_PASSWORD", config.password);
+        secret_config.insert("WALLABAG_URL", config.base_url);
 
-        for (key, val) in secret_config.iter() {
+        for (&key, val) in secret_config.iter() {
             collection.create_item(
                 &format!("Read It Later account: {}", username),
-                vec![("wallabag_username", &username), ("attr", &key)],
+                HashMap::from([("wallabag_username", username), ("attr", key)]),
                 &val.clone().into_bytes(),
                 false,
                 "text/plain",
@@ -55,7 +54,7 @@ impl SecretManager {
         Ok(())
     }
 
-    pub fn is_logged(username: &str) -> Result<Config, SsError> {
+    pub fn is_logged(username: &str) -> Result<Config, Error> {
         let service = Self::new()?;
         let client_id = service.retrieve(username, "WALLABAG_CLIENT_ID")?;
         let client_secret = service.retrieve(username, "WALLABAG_CLIENT_SECRET")?;
@@ -71,12 +70,12 @@ impl SecretManager {
         })
     }
 
-    fn retrieve(&self, key: &str, attribute: &str) -> Result<String, SsError> {
+    fn retrieve(&self, key: &str, attribute: &str) -> Result<String, Error> {
         let items = self.service.search_items(vec![("wallabag_username", key), ("attr", attribute)])?;
         if let Some(item) = items.get(0) {
             let value = item.get_secret()?;
             return Ok(String::from_utf8(value).unwrap());
         }
-        Err(SsError::NoResult)
+        Err(Error::NoResult)
     }
 }
