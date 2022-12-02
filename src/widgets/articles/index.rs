8206a79efed8231c83dfba1f69e099dd19e79109
Casper Meijn <casper@meijn.net>
2022-12-02 20:05:49 +0100

    Use obj() instead of instance()

--- src/widgets/articles/index.rs
+++ src/widgets/articles/index.rs
@@ -53,7 +53,7 @@ mod imp {
 
     impl ObjectImpl for ArticleWidget {
         fn dispose(&self) {
-            while let Some(child) = self.instance().first_child() {
+            while let Some(child) = self.obj().first_child() {
                 child.unparent();
             }
         }
